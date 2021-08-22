8c3a20c92e7bce2d66e9f05791610e4dbfb3d5cc
Anmol Ahuja <55618052+eat-them@users.noreply.github.com>
2021-08-23 01:51:38 +0530

    Update urls.py

--- onlinequiz/urls.py
+++ onlinequiz/urls.py
@@ -7,6 +7,7 @@ from django.contrib.auth.views import LogoutView,LoginView
 from django.views.static import serve
 from django.conf.urls import url 
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
    
@@ -48,5 +49,5 @@ urlpatterns = [
    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
